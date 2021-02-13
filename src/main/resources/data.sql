9722a0662440d092523759c908677e78a9e21df2
nagarjunaboddu <nagarjuna.boddu@cognizant.com>
2021-02-13 16:18:10 -0500

    Auto Deployment

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO employee (emp_id, first_name, last_name) VALUES (7, 'Steve', 'boro');
+INSERT INTO employee (emp_id, first_name, last_name) VALUES (8, 'Lux', 'carlos');
+INSERT INTO employee (emp_id, first_name, last_name) VALUES (9, 'Mellisa', 'camp');
+
+INSERT INTO email_address (email_id, email_type, email,employee_id) VALUES (7, 'personal', 'personal1@gmail.com',7);
+INSERT INTO email_address (email_id, email_type, email,employee_id) VALUES (8, 'official', 'official1@gmail.com',7);
+INSERT INTO email_address (email_id, email_type, email,employee_id) VALUES (9, 'personal', 'personal2@gmail.com',8);
+INSERT INTO email_address (email_id, email_type, email,employee_id) VALUES (10, 'official', 'official2@gmail.com',8);
+INSERT INTO email_address (email_id, email_type, email,employee_id) VALUES (11, 'personal', 'personal3@gmail.com',9);
+INSERT INTO email_address (email_id, email_type, email,employee_id) VALUES (12, 'official', 'official3@gmail.com',9);
+
