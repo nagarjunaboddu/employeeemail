13395f59ea3e19ae74da1b7b230063a24597fcb0
nagarjunaboddu <nagarjuna.boddu@cognizant.com>
2021-02-13 12:18:04 -0500

    first commit

---
+++ src/main/java/com/galvanize/employeeemail/EmployeeemailApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.employeeemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeemailApplication.class, args);
+	}
+
+}
